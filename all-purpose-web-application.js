1a7257e97d60c2f71bc74b6207233eedcb9ef0a0
sakujyo <tomo3u3u@gmail.com>
2021-07-13 06:57:14 +0900

    modify CSS styles

--- all-purpose-web-application.js
+++ all-purpose-web-application.js
@@ -20,7 +20,7 @@ http.createServer((req, res) => {
 	}
 	if (req.url == '/listen' && req.method == 'GET') {
 		res.writeHeader(200, { 'Content-Type': 'text/html' });
-		res.write('<form action="/load" method="POST"><input name="port" placeholder="input port" /><br /><textarea name="app" cols="80" rows="24"></textarea><br /><input type="submit" value="listen" /></form>');
+		res.write('<style>* { font: 14pt Consolas; } textarea { width: 100%; }</style><form action="/load" method="POST"><input name="port" placeholder="port" /><br /><textarea name="app" rows="24" wrap="off" placeholder="code"></textarea><br /><input type="submit" value="listen" /></form>');
 		res.end();
 	}
 }).listen(process.argv[2]);
